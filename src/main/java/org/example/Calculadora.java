e28cf8a4094b2419af38488251d6291344af15b5
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2025-03-24 21:18:16 -0300

    Adicionando divisao, multiplicacao, potencia

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -5,30 +5,37 @@ public class Calculadora {
         System.out.println("Hello, World!");
     }
 
-    public int somar ( int a, int b){
+    public int somar(int a, int b) {
         return a + b;
     }
 
-    public int subtrair ( int a, int b){
+    public int subtrair(int a, int b) {
         return a - b;
     }
 
-    public int multiplicar ( int a, int b){
+    public int multiplicar(int a, int b) {
         return a * b;
     }
 
-    public double dividir ( int a, int b){
+    public double dividir(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Nenhum número pode ser divido por zero");
         }
         return (double) a / b;
     }
 
-
-    public double potencia ( int base, int expoente){
-        if (base == 0 && expoente == 0) {
-            throw new ArithmeticException("Indefinição matemática: 0^0 não pode ser calculado. Porque todo número elevado a 0 é 1");
+    public double potencia(int base, int expoente) {
+        if (expoente == 0 && base != 0) {
+            throw new ArithmeticException("Todo número elevado a 0 é 1");
         }
         return Math.pow(base, expoente);
     }
+
+    public double potenciaExpoenteZero(int base, int expoente) {
+        if (base == 0 && expoente == 0) {
+            throw new ArithmeticException("Indefinição matemática: 0^0 não pode ser calculado.");
+        }
+        return Math.pow(base, expoente);
+    }
+
 }
--- src/test/java/calculadora/testes/unitarios/CalculadoraTesteJUnit.java
+++ src/test/java/calculadora/testes/unitarios/CalculadoraTesteJUnit.java
@@ -12,13 +12,39 @@ public class CalculadoraTesteJUnit {
     void testeSoma() {
         Calculadora calculadora = new Calculadora();
         assertEquals(3, calculadora.somar(1, 2));
-        assertEquals(0, calculadora.somar(-1, 1));
+        assertEquals(10, calculadora.somar(11, 5));
     }
 
     @Test
     void testeSubtracao(){
         Calculadora calculadora = new Calculadora();
         assertEquals(9, calculadora.subtrair(10, 1));
+        assertEquals(5, calculadora.subtrair(10, 7));
+    }
 
+    @Test
+    void testeMultiplicacao (){
+        Calculadora calculadora = new Calculadora();
+        assertEquals(15, calculadora.multiplicar(5, 3));
+        assertEquals(20, calculadora.multiplicar(5, 5));
+    }
+
+    @Test
+    void testeDivisao (){
+        Calculadora calculadora = new Calculadora();
+        assertEquals(5, calculadora.dividir(10,2));
+        assertEquals(3, calculadora.dividir(6,0));
+    }
+
+    @Test
+    void testePotencia(){
+        Calculadora calculadora = new Calculadora();
+        assertEquals(8, calculadora.potencia(2, 3));
+        assertEquals(9, calculadora.potencia(3, 0));
+    }
+    @Test
+    void testePotenciaExpoenteZero(){
+        Calculadora calculadora = new Calculadora();
+        assertEquals(9, calculadora.potenciaExpoenteZero(0, 0));
     }
 }
